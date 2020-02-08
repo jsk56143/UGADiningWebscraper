41a5fcdc9e0df2052b9d11e895e6dbe15c52bf0d
jsk56143 <jsk56143@uga.edu>
2020-02-08 13:03:51 -0500

    Renamed file

--- src/Webscraper.java
+++ src/Webscraper.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class Webscraper {
     public static void main(String[] args) {
         System.out.println("Hello people");
         System.out.println("Hello from George!");
