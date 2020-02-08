c92d55f0beb9393a24ad133881d3f3275f2fde24
jsk56143 <jsk56143@uga.edu>
2020-02-08 13:23:31 -0500

    Created basis for webscraper

--- src/Webscraper.java
+++ src/Webscraper.java
@@ -1,7 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
 public class Webscraper {
     public static void main(String[] args) {
-        System.out.println("Hello people");
-        System.out.println("Hello from George!");
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://apps.auxiliary.uga.edu/Dining/BuildYourPlate/index.php/nutrition").get();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (doc != null){
+            System.out.println("It works!");
+        }
+
     }
     //test
 }
